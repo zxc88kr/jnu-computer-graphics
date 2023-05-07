dd59e8d5327c3f501dab14f4adeb35e0aa69b508
zxc88kr <zxc88kr@naver.com>
2023-05-07 23:38:38 +0900

    HW3 Description

--- src/HW3.cpp
+++ src/HW3.cpp
@@ -4,7 +4,7 @@
 
 #include <cstdio>
 
-struct Vector3d // 3D Vector
+struct Vector3d // 3D vector
 {
 	double x; // x-coordinate
 	double y; // y-coordinate
@@ -14,12 +14,13 @@ struct Vector3d // 3D Vector
 	Vector3d() : x(0), y(0), z(0) {}
 	Vector3d(double _x, double _y, double _z) : x(_x), y(_y), z(_z) {}
 
-	
+	// print vector elements
 	friend void printVector3d(const Vector3d& a)
 	{
 		printf("(%lf, %lf, %lf)\n", a.x, a.y, a.z);
 	}
 
+	// operator overloading
 	friend Vector3d operator+(const Vector3d& a, const Vector3d& b)
 	{
 		double _x = a.x + b.x;
@@ -55,6 +56,8 @@ struct Vector3d // 3D Vector
 		double _z = a.z / d;
 		return Vector3d(_x, _y, _z);
 	}
+
+	// cross & dot product
 	friend Vector3d cross(const Vector3d& a, const Vector3d& b)
 	{
 		double _x = a.y * b.z - a.z * b.y;
@@ -84,16 +87,20 @@ int main()
 	double k;
 	Vector3d N, U, V;
 
+	// N = P0P1 × P0P2
 	N = cross(P1 - P0, P2 - P0);
 	printf("N = ");
 	printVector3d(N);
 
+	// k = P0P1 × P0P2 · N
 	k = dot(cross(P1 - P0, P2 - P0), N);
 
+	// U = P0P2 × N / k
 	U = cross(P2 - P0, N) / k;
 	printf("U = ");
 	printVector3d(U);
 
+	// V = N × P0P1 / k
 	V = cross(N, P1 - P0) / k;
 	printf("V = ");
 	printVector3d(V);
@@ -104,36 +111,46 @@ int main()
 	bool isHit;
 	Vector3d v, P;
 
+	// v = OV
 	v = V1 - O;
 
+	// t = ((P0 - O) · N) / (v · N)
 	t = dot(P0 - O, N) / dot(v, N);
 
+	// P = O + t * v
 	P = O + t * v;
 	printf("P = ");
 	printVector3d(P);
 
+	// u = P0P · U, v = P0P · V
 	_u = dot(P - P0, U);
 	_v = dot(P - P0, V);
 	printf("u, v = (%lf, %lf)\n", _u, _v);
 
+	// u > 0 && v > 0 && u + v < 1
 	isHit = (_u > 0 && _v > 0 && _u + _v < 1) ? true : false;
 	if (isHit) printf("ray hits triangle\n");
 	else	   printf("ray doesn't hit triangle\n");
 
 	printf("----------------------------------------\n");
 
+	// v = OV
 	v = V2 - O;
 
+	// t = ((P0 - O) · N) / (v · N)
 	t = dot(P0 - O, N) / dot(v, N);
 
+	// P = O + t * v
 	P = O + t * v;
 	printf("P = ");
 	printVector3d(P);
 
+	// u = P0P · U, v = P0P · V
 	_u = dot(P - P0, U);
 	_v = dot(P - P0, V);
 	printf("u, v = (%lf, %lf)\n", _u, _v);
 
+	// u > 0 && v > 0 && u + v < 1
 	isHit = (_u > 0 && _v > 0 && _u + _v < 1) ? true : false;
 	if (isHit) printf("ray hits triangle\n");
 	else	   printf("ray doesn't hit triangle\n");
